2c6b6ebbbbe0a57831f977a870a8c3ecbf82bcf6
Gaston Dvoskin <dvoskingaston@gmail.com>
2024-08-06 07:08:19 -0300

    Developed ParagraphsContainer and used it in WeAre.tsx. Next step: add texts into assets

--- src/sections/WeAre.tsx
+++ src/sections/WeAre.tsx
@@ -1,6 +1,12 @@
+import ParagraphsContainer from "../components-reusable/ParagraphsContainer"
+import weAreParagraphs from "../assets/texts/weAre"
+
 const WeAre = () => {
   return (
-    <div>WeAre</div>
+    <div>
+      <h2>SOMOS</h2>
+      <ParagraphsContainer paragraphs={weAreParagraphs}/>
+    </div>
   )
 }
 
