37d5a5a15e2750019c2fde6b2180d8e7d671f0f5
Gaston Dvoskin <dvoskingaston@gmail.com>
2024-08-07 15:51:09 -0300

    Added images in Nav

--- src/sections/Nav.tsx
+++ src/sections/Nav.tsx
@@ -1,9 +1,14 @@
-import SectionHeader from "../components-reusable/SectionHeader";
+import SectionHeader from "../components-reusable/SectionHeader"; 
+import ImageComponent from "../components-reusable/ImageComponent";
+import logo from "../assets/images/enterprise/logo.webp"; 
+import logoVertical from "../assets/images/enterprise/logo-vertical.webp"; 
 
 const Nav = () => {
   return (
     <nav>
       <SectionHeader text="NAV" />
+      <ImageComponent src={logo} alt="logo" />
+      <ImageComponent src={logoVertical} alt="Logo Vertical" />
     </nav>
   );
 };
