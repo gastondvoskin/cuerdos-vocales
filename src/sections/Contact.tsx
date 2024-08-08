9ccf052f9e4d72557ebd05025efdf33ea87f6505
Gaston Dvoskin <dvoskingaston@gmail.com>
2024-08-08 06:17:05 -0300

    Installed React-icons. Imported icons in Contact.tsx. Updated mail anchor. Next step: update the other icons in Contact.tsx and modularize

--- src/sections/Contact.tsx
+++ src/sections/Contact.tsx
@@ -1,6 +1,8 @@
 import ParagraphsContainer from "../components-reusable/ParagraphsContainer";
 import SectionHeader from "../components-reusable/SectionHeader";
 import { contactParagraphs } from "../assets/texts/sections";
+import { SiGmail } from "react-icons/si";
+import { FaInstagram, FaFacebook, FaYoutube, FaSpotify } from 'react-icons/fa';
 import { MAIL, INSTAGRAM, FACEBOOK, YOUTUBE, SPOTIFY } from "../constants/contact";
 
 
@@ -9,11 +11,20 @@ const Contact = () => {
     <section>
       <SectionHeader text="REDES Y CONTACTO" />
       <ParagraphsContainer paragraphs={contactParagraphs} />
-      <a href={MAIL}>Mail</a>
-      <a href={INSTAGRAM}>Instagram</a>
-      <a href={FACEBOOK}>Facebook</a>
-      <a href={YOUTUBE}>Youtube</a>
-      <a href={SPOTIFY}>Spotify</a>
+      <a 
+        href={`mailto:${MAIL}`}
+        target="_blank"
+        rel="noopeneer noreferrer"
+      >
+        <SiGmail />
+      </a>
+
+
+
+      <a href={INSTAGRAM}><FaInstagram /></a>
+      <a href={FACEBOOK}><FaFacebook /></a>
+      <a href={YOUTUBE}><FaYoutube /></a>
+      <a href={SPOTIFY}><FaSpotify /></a>
     </section>
   );
 };
