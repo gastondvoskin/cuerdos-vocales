b84930d2880dbff019dc607066eb49a9c6d79971
Gaston Dvoskin <dvoskingaston@gmail.com>
2024-08-09 06:18:54 -0300

    Renamed SmallNav.tsx to LargeNav.tsx. Created NavSmall.tsx. Next step: add bars icon

--- src/navigation/NavSmall.tsx
+++ src/navigation/NavSmall.tsx
@@ -1,22 +1,28 @@
 import ImageComponent from "../components-reusable/ImageComponent";
 import logo from "../assets/images/enterprise/logo.webp";
-import logoVertical from "../assets/images/enterprise/logo-vertical.webp"; 
+import logoVertical from "../assets/images/enterprise/logo-vertical.webp";
 import TARGETS from "../constants/sections";
 import NavLink from "./NavLink";
+import { useState } from "react";
 
 const NavSmall = () => {
+  const [openMenu, setOpenMenu] = useState<boolean>(false);
+
   return (
     <header>
       <nav>
         <ImageComponent src={logo} alt="logo" />
         <ImageComponent src={logoVertical} alt="Logo Vertical" />
-        <ul>
-          {TARGETS.map((target, index: number) => (
-            <li>
-              <NavLink key={index} target={target} />
-            </li>
-          ))}
-        </ul>
+        <button onClick={() => {setOpenMenu(!openMenu)}}>MENU (BARS)</button>
+        {openMenu && (
+          <ul>
+            {TARGETS.map((target, index: number) => (
+              <li>
+                <NavLink key={index} target={target} />
+              </li>
+            ))}
+          </ul>
+        )}
       </nav>
     </header>
   );
