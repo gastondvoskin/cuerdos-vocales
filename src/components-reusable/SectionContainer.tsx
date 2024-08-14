b1231b1ba6b3c6c6b7a836410089543fce51de1e
Gaston Dvoskin <dvoskingaston@gmail.com>
2024-08-14 10:30:54 -0300

    Added background to Home.tsx. Next step: position images and album in Home.tsx

--- src/components-reusable/SectionContainer.tsx
+++ src/components-reusable/SectionContainer.tsx
@@ -1,11 +1,21 @@
 import { ISectionContainer } from "../types";
 
-const SectionContainer: React.FC<ISectionContainer> = ({ sectionId, children }) => {
+const SectionContainer: React.FC<ISectionContainer> = ({
+  sectionId,
+  children,
+}) => {
   return (
-    <section id={sectionId} className={`pt-20 bg-gradient-to-b from-myPink-500 to-myPurple-100`}>
+    <section
+      id={sectionId}
+      className={`pt-20  bg-[conic-gradient(at_top,_#f4ded8,_#eaeaea)]`}
+    >
       {children}
     </section>
   );
 };
 
 export default SectionContainer;
+
+/* OLD
+bg-gradient-to-b from-myPink-500 to-myPurple-100
+*/
--- src/sections/Home.tsx
+++ src/sections/Home.tsx
@@ -9,10 +9,13 @@ import SectionContainer from "../components-reusable/SectionContainer";
 const Home = () => {
   return (
     <SectionContainer sectionId="home">
-      <div className="flex flex-col items-center">
-        <ImageComponent src={logoVertical} alt="Logo Cuerdos Vocales" className="h-40" />
-        <p>Grupo vocal de folklore</p>
-        <p>Ganador Pre Cosquín</p>
+      <div className="flex flex-col items-center mt-14">
+        <ImageComponent src={logoVertical} alt="Logo Cuerdos Vocales" className="w-64 border-10" />
+        {/* reference: https://design2tailwind.com/blog/tailwindcss-gradient-text/ */}
+        <div className="text-sm bg-gradient-to-r from-myBrown-900 via-myBrown-100 to-myOrange-900 inline-block text-transparent bg-clip-text" text-grad>
+          <p>Grupo vocal de folklore</p>
+          <p>Ganador Pre Cosquín</p>        
+        </div>
       </div>
       <div>
         <ImageComponent src={dress} alt="Pollera de bailarina" />
