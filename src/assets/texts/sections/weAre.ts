a1f83d6d3f666960faa6d7b8ec30b9f1298ff926
Gaston Dvoskin <dvoskingaston@gmail.com>
2024-08-15 12:36:27 -0300

    Implemented and styled view more button in WeAre.tsx. In the future modularize the button

--- src/assets/texts/sections/weAre.ts
+++ src/assets/texts/sections/weAre.ts
@@ -1,5 +1,7 @@
 const weAreParagraphs = [
   "Cuerdos Vocales es una agrupación musical fundada por Gastón Dvoskin en 2014 con el fin de explorar la fuerza del canto colectivo en el folklore argentino. Ha realizado ciclos de conciertos, peñas y talleres, presentándose junto a artistas de renombre como Juan Quintero, Teresa Parodi y La Charo. ", 
+  
+  "La formación actual cuenta con Gastón Dvoskin (dirección), Alejandro Randazzo (voz y producción), Bernardo Guarrochena (voz y producción), Manuel Estrach (voz), Lucrecia Flores (voz), Aldana Gómez (voz), Paula Bori (voz), Catalina Telerman (voz). ", 
 
   "En enero de 2020, Cuerdos Vocales se consagró como ganador del Pre Cosquín en la categoría conjunto vocal, lo que le permitió participar en la 60a edición del Festival Nacional de Folklore de Cosquín, donde actuó en el escenario Atahualpa Yupanqui. ", 
 
@@ -7,7 +9,7 @@ const weAreParagraphs = [
 
   "Cuerdos Vocales se ha presentado en importantes salas como Usina del Arte, Caras y Caretas, Café Vinilo, Centro Nacional de la Música y Teatro Nacional Cervantes. ",
 
-  "La formación actual cuenta con Gastón Dvoskin (dirección), Alejandro Randazzo (voz y producción), Bernardo Guarrochena (voz y producción), Manuel Estrach (voz), Lucrecia Flores (voz), Aldana Gómez (voz), Paula Bori (voz), Catalina Telerman (voz). ", 
+
 
   // "Redes: instagram.com/cuerdosvocales - facebook.com/cuerdosvocales - www.cuerdosvocales.com ",
 ]
--- src/sections/WeAre.tsx
+++ src/sections/WeAre.tsx
@@ -5,13 +5,26 @@ import ImageComponent from "../components-reusable/ImageComponent";
 import circular2 from "../assets/images/photos/circular-2.webp";
 import SectionContainer from "../components-reusable/SectionContainer";
 import { SECTIONS } from "../constants/sections";
+import { useState } from "react";
 
 
 const WeAre = () => {
+  const [ numberOfParagraphs, setNumberOfParagraphs ] = useState<number>(2);
+  const isDisabled = numberOfParagraphs >= weAreParagraphs.length;
+
   return (
     <SectionContainer section={SECTIONS.WE_ARE}>
       <SectionHeader text="SOMOS" />
-      <ParagraphsContainer paragraphs={weAreParagraphs} />
+      <ParagraphsContainer paragraphs={weAreParagraphs.slice(0, numberOfParagraphs)} />
+      {!isDisabled && (
+        <button
+          disabled={isDisabled}
+          className={`bg-myWhite-500 px-3 py-2 rounded-2xl shadow-lg text-myOlive-100 hover:text-myOlive-100 hover:bg-myWhite-500 disabled:opacity-50 mb-10`}
+          onClick={() => setNumberOfParagraphs(weAreParagraphs.length)}
+        >
+          Leer más
+        </button>)   
+      }
       <ImageComponent src={circular2} alt="Integrantes de Cuerdos Vocales mirando a cámara" />
     </SectionContainer>
   );
