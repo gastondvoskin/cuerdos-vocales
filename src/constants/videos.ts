3fd38bee25a0d4485d7a2d8f372614e2c2f52c82
Gaston Dvoskin <dvoskingaston@gmail.com>
2024-08-15 12:06:35 -0300

    Implemented button in Videos.tsx. Modularize in the future to reuse it in Album.tsx. Next step: style WeAre.tsx

--- src/constants/videos.ts
+++ src/constants/videos.ts
@@ -11,7 +11,61 @@ const videos = [
     label: "Andando ft. Juan Quintero",
     src: "https://www.youtube.com/embed/ermyCbeIR7s?si=2bxuQdN7e-UxvYbW",
   },
+  {
+    label: "Charrada de los perros ft. Juan Quintero",
+    src: "https://www.youtube.com/embed/RDw9Xzi1S3U?si=eOOqd48notcE8nf3",
+  },
+  {
+    label: "El Guayaboso en la Usina del Arte",
+    src: "https://www.youtube.com/embed/hMvtXUJXzbY?si=jKYHY_sfZXLjX8lo",
+  },
+  {
+    label: "Chacarera santiagueña en Espacio Tucumán",
+    src: "https://www.youtube.com/embed/Ja8TcVmRq0M?si=MUEn33ByW4wCZwsu",
+  },
+  {
+    label: "Cuando muere el angelito ft. Nadia Larcher",
+    src: "https://www.youtube.com/embed/xyoaVaOAF08?si=M8W_q2Evruio5sD0",
+  },
+  {
+    label: "Doña Ubenza",
+    src: "https://www.youtube.com/embed/Usn2CIbkkdk?si=sqtTdxaKnyy0QHqh",
+  },
+  {
+    label: "Esa fulanita",
+    src: "https://www.youtube.com/embed/z6GIo-xSFmY?si=DxyTAuFKqaXIwgt5",
+  },
+  {
+    label: "Canción de las cantinas ft. Nacho Vidal y Nadia Szachniuk",
+    src: "https://www.youtube.com/embed/CEjClaV7KsU?si=VUc4EK1lQMu0cP_D",
+  },
+  {
+    label: "La Angelita Rosales en Cosquín",
+    src: "https://www.youtube.com/embed/3MXFKl37DHE?si=bt4EeRDOqv_e0C5t",
+  },
+  {
+    label: "Canción de las cantinas ft. Nacho Vidal y Nadia Szachniuk",
+    src: "https://www.youtube.com/embed/CEjClaV7KsU?si=VUc4EK1lQMu0cP_D",
+  },
+  {
+    label: "Alajita ft. Juan Quintero",
+    src: "https://www.youtube.com/embed/3qYzsty_0Wk?si=6J51jhT9mHvUoAIF",
+  },
+  {
+    label: "Arriba quemando el Sol ft. Milena Salamanca",
+    src: "https://www.youtube.com/embed/4KoyawdIVfo?si=FJITf_T4Ds_P9_Fj",
+  },
+  {
+    label: "Canción de las cantinas ft. Nacho Vidal y Nadia Szachniuk",
+    src: "https://www.youtube.com/embed/CEjClaV7KsU?si=VUc4EK1lQMu0cP_D",
+  },
+  {
+    label: "Sumaj Pachamama ft. La Charo",
+    src: "https://www.youtube.com/embed/AfF-wa9TPmc?si=viY6dqo6gMr2y6h7",
+  },
 ];
+
+
 
 const videoCosquin = {
   label: "Andando en Cosquín",
--- src/sections/Videos.tsx
+++ src/sections/Videos.tsx
@@ -1,4 +1,4 @@
-import SectionHeader from "../components-reusable/SectionHeader"; 
+import SectionHeader from "../components-reusable/SectionHeader";
 import VideoComponent from "../components-reusable/VideoComponent";
 import { videos } from "../constants/videos";
 // import Button from "../components-reusable/Button";
@@ -6,16 +6,25 @@ import SectionContainer from "../components-reusable/SectionContainer";
 import { SECTIONS } from "../constants/sections";
 import { useState } from "react";
 
-
 const Videos = () => {
-  const [ loadMore, setLoadMore ] = useState(false);
+  const [numberOfVideos, setNumberOfVideos] = useState<number>(1);
+  const isDisabled = numberOfVideos >= videos.length;
+
   return (
     <SectionContainer section={SECTIONS.VIDEOS}>
       <SectionHeader text="VIDEOS" />
       <div className="w-full">
-        {videos.map((video, index) => <VideoComponent video={video} key={index} />)}
+        {videos.slice(0, numberOfVideos).map((video, index) => (
+          <VideoComponent video={video} key={index} />
+        ))}
       </div>
-      <button className="bg-myOlive-100 px-3 py-2 rounded-2xl shadow-lg text-myWhite-500 hover:text-myOlive-100 hover:bg-myWhite-500" onClick={() => setLoadMore(!loadMore)}>{loadMore ? "Cargar menos videos": "Cargar más videos"}</button>
+      <button
+        disabled={isDisabled}
+        className={`bg-myOlive-100 px-3 py-2 rounded-2xl shadow-lg text-myWhite-500 hover:text-myOlive-100 hover:bg-myWhite-500 disabled:opacity-50`}
+        onClick={() => setNumberOfVideos(numberOfVideos + 2)}
+      >
+        {isDisabled ? "Todos los videos cargados" : "Cargar más videos"}
+      </button>
 
       {/* <Button text="Cargar más videos"/> */}
     </SectionContainer>
