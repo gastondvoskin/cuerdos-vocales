d9762a025816f9c9b2f5d757fadcadfc3fef190e
Gaston Dvoskin <dvoskingaston@gmail.com>
2024-08-15 14:19:40 -0300

    Fixed circularConcert constant link

--- src/sections/CircularConcert.tsx
+++ src/sections/CircularConcert.tsx
@@ -11,13 +11,7 @@ const CircularConcert = () => {
     <SectionContainer section={SECTIONS.CIRCULAR_CONCERT}>
       <SectionHeader text="CONCIERTO CIRCULAR" />
       <ImageComponent src={flyer1} alt="Pintura en acrílico con cantantes sentados en ronda en un pícnic" className="w-56 rounded-full aspect-square my-3 animate-spin z-0" />
-      {/* <ImageComponent src={album} alt="Concierto circular" className="w-40 rounded-full aspect-square my-3 animate-spin z-0" /> */}
       <div className="animate-fadeIn text-center text-xs">
-        {/* <p>En el centro de la noche,<br />
-        las voces se entrelazan,<br />
-        un círculo de magia<br />
-        donde la música despierta.</p>
-        <br /> */}
         <p>Lo ancestral y lo nuevo<br />
         se abrazan en armonía,<br />
         y en circular encuentro,<br />
