e982bb21f9c04cb049f060a361935936ca05237b
Gaston Dvoskin <dvoskingaston@gmail.com>
2024-08-15 10:59:26 -0300

    Styled button in Videos.tsx. Next step: implement functionality to button and modularize it

--- src/sections/Videos.tsx
+++ src/sections/Videos.tsx
@@ -1,19 +1,23 @@
 import SectionHeader from "../components-reusable/SectionHeader"; 
 import VideoComponent from "../components-reusable/VideoComponent";
 import { videos } from "../constants/videos";
-import Button from "../components-reusable/Button";
+// import Button from "../components-reusable/Button";
 import SectionContainer from "../components-reusable/SectionContainer";
 import { SECTIONS } from "../constants/sections";
+import { useState } from "react";
 
 
 const Videos = () => {
+  const [ loadMore, setLoadMore ] = useState(false);
   return (
     <SectionContainer section={SECTIONS.VIDEOS}>
       <SectionHeader text="VIDEOS" />
       <div className="w-full">
         {videos.map((video, index) => <VideoComponent video={video} key={index} />)}
       </div>
-      <Button text="Cargar más videos"/>
+      <button className="bg-myOlive-100 px-3 py-2 rounded-2xl shadow-lg text-myWhite-500 hover:text-myOlive-100 hover:bg-myWhite-500" onClick={() => setLoadMore(!loadMore)}>{loadMore ? "Cargar menos videos": "Cargar más videos"}</button>
+
+      {/* <Button text="Cargar más videos"/> */}
     </SectionContainer>
   );
 };
