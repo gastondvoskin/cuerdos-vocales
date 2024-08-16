85aee5191167073589a2f3f0460e0c6f13a9f83f
Gaston Dvoskin <dvoskingaston@gmail.com>
2024-08-16 07:16:00 -0300

    Refactored WeAre.tsx. Added useEffect to handle state

--- src/sections/WeAre.tsx
+++ src/sections/WeAre.tsx
@@ -9,21 +9,34 @@ import { useState } from "react";
 import Button from "../components-reusable/Button";
 import { MdExpandMore } from "react-icons/md";
 
-
-
-
 const WeAre = () => {
-  const [ numberOfParagraphs, setNumberOfParagraphs ] = useState<number>(2);
-  const renderButton = numberOfParagraphs !== weAreParagraphs.length;
+  const [numberOfParagraphs, setNumberOfParagraphs] = useState<number>(2);
+  const [renderButton, setRenderButton] = useState<boolean>(true);
+
+  const handleViewMore = () => {
+    setNumberOfParagraphs(weAreParagraphs.length); 
+    setRenderButton(false);
+  }; 
 
   return (
     <SectionContainer section={SECTIONS.WE_ARE}>
       <SectionHeader text="SOMOS" />
       <ParagraphsContainer paragraphs={weAreParagraphs.slice(0, numberOfParagraphs)} />
       {renderButton && (
-        <Button text="Leer más" bgColor="myWhite-500" textColor="myPurple-100" onClick={() => setNumberOfParagraphs(weAreParagraphs.length)} Icon={MdExpandMore} className="mb-6" />)  
-      }
-      <ImageComponent src={circular2} alt="Integrantes de Cuerdos Vocales mirando a cámara" className="w-full h-44 object-cover"/>
+        <Button
+          text="Leer más"
+          bgColor="myWhite-500"
+          textColor="myPurple-100"
+          onClick={handleViewMore}
+          Icon={MdExpandMore}
+          className="mb-6"
+        />
+      )}
+      <ImageComponent
+        src={circular2}
+        alt="Integrantes de Cuerdos Vocales mirando a cámara"
+        className="w-full h-44 object-cover"
+      />
     </SectionContainer>
   );
 };
