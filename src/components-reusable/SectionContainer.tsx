e2e7f531c4567f8ad809411e9921e91579166cb8
Gaston Dvoskin <dvoskingaston@gmail.com>
2024-08-21 18:57:08 -0300

    Responsive WeAre.tsx

--- src/components-reusable/SectionContainer.tsx
+++ src/components-reusable/SectionContainer.tsx
@@ -9,10 +9,9 @@ const SectionContainer: React.FC<ISectionContainer> = ({ section, children, /* c
     <section
       id={section}
       className={`
-        flex flex-col items-center
+        flex flex-col items-center pb-10
         ${orangeBgSections.includes(section) && "bg-[conic-gradient(at_top,_#f5ddd6,_#e99981)]"}
-        ${purpleBgSections.includes(section) && "bg-[conic-gradient(at_top,_#b4acbe,_#9052ce)]"} 
-        ${section === SECTIONS.WE_ARE ? "pb-0" : "pb-10"}
+        ${purpleBgSections.includes(section) && "bg-[conic-gradient(at_top,_#b4acbe,_#9052ce)]"}       
         ${section === SECTIONS.CONTACT ? "pt-10" : "pt-16"}
         ${section === SECTIONS.HOME && "pt-5 pb-0"}
       `}
@@ -25,6 +24,7 @@ const SectionContainer: React.FC<ISectionContainer> = ({ section, children, /* c
 export default SectionContainer; 
 
 
-// 
+// OLD
 // ${orangeBgSections.includes(section) && "bg-[conic-gradient(at_top,_#fad5cb,_#ffffff)]"}
 // ${purpleBgSections.includes(section) && "bg-[conic-gradient(at_top,_#eadffa,_#fcd3c8)]"} 
+// ${section === SECTIONS.WE_ARE ? "pb-0" : "pb-10"}
