8e41365f1bb8fd43e9b33247cb9f278d7793db96
Gaston Dvoskin <dvoskingaston@gmail.com>
2024-08-21 10:18:11 -0300

    Added icons to NavLink. Next step: pick better options

--- src/constants/sections.ts
+++ src/constants/sections.ts
@@ -1,5 +1,8 @@
 // const TARGETS = ["home", "news", "videos", "weAre", "album", "cosquin", "cordura", "circularConcert", "contact"];
 import { ITarget } from "../types";
+// import { FaBars } from "react-icons/fa6";
+import { FaNewspaper, FaVideo, FaUsers, FaMusic, FaGuitar, FaBrain, FaTheaterMasks, FaEnvelope } from 'react-icons/fa';
+
 
 export const SECTIONS = {
   HOME: "home",
@@ -16,12 +19,12 @@ export const SECTIONS = {
 
 export const TARGETS: ITarget[] = [
   // { id: SECTIONS.HOME, text: "INICIO" },
-  { id: SECTIONS.NEWS, text: "NOVEDADES" },
-  { id: SECTIONS.VIDEOS, text: "VIDEOS" },
-  { id: SECTIONS.WE_ARE, text: "SOMOS" },
-  { id: SECTIONS.ALBUM, text: "ÁLBUM" },
-  { id: SECTIONS.COSQUIN, text: "COSQUÍN" },
-  { id: SECTIONS.CORDURA, text: "CORDURA" },
-  { id: SECTIONS.CIRCULAR_CONCERT, text: "XIRGU" },
-  { id: SECTIONS.CONTACT, text: "CONTACTO" },
+  { id: SECTIONS.NEWS, text: "NOVEDADES", icon: FaNewspaper },
+  { id: SECTIONS.VIDEOS, text: "VIDEOS", icon: FaVideo },
+  { id: SECTIONS.WE_ARE, text: "SOMOS", icon: FaUsers },
+  { id: SECTIONS.ALBUM, text: "ÁLBUM", icon: FaMusic },
+  { id: SECTIONS.COSQUIN, text: "COSQUÍN", icon: FaGuitar },
+  { id: SECTIONS.CORDURA, text: "CORDURA", icon: FaBrain },
+  { id: SECTIONS.CIRCULAR_CONCERT, text: "XIRGU", icon: FaTheaterMasks },
+  { id: SECTIONS.CONTACT, text: "CONTACTO", icon: FaEnvelope },
 ]; 
--- src/navigation/NavLink.tsx
+++ src/navigation/NavLink.tsx
@@ -1,23 +1,27 @@
 import { Link } from "react-scroll";
-import { INavLinkProps } from "../types";
+import { INavLinkProps } from "../types"; 
 
 const NavLink: React.FC<INavLinkProps> = ({ target, currentSection, setCurrentSection, setOpenMenu }) => {
   const isCurrent: boolean = target.id === currentSection;
 
+  const IconComponent = target.icon;
   return (
-    <Link
-      to={target.id} 
-      onClick={() => {
-        setCurrentSection(target.id); 
-        setOpenMenu && setOpenMenu(false); // LargeNav does not have setOpenMenu
-      }}
-      smooth={true}
-      duration={300}
-      className={`${isCurrent ? "border-b-2 border-myOlive-900 text-myOlive-900" : " text-myPurple-500"} hover:bg-myPurple-100 hover:text-myWhite-500 cursor-pointer`}
-    >
-      {target.text}
-    </Link>
+    <div className={`flex items-center gap-2 ${isCurrent ? "border-b-2 border-myOlive-900 text-myOlive-900" : " text-myPurple-500"} hover:bg-myPurple-100 hover:text-myWhite-500 cursor-pointer`} >
+      <IconComponent />
+      <Link
+        to={target.id} 
+        onClick={() => {
+          setCurrentSection(target.id); 
+          setOpenMenu && setOpenMenu(false); // LargeNav does not have setOpenMenu
+        }}
+        smooth={true}
+        duration={300}
+        
+      >
+        {target.text}
+      </Link>
+    </div>
   );
 };
 
-export default NavLink;
+export default NavLink; 
