393230e7f74901bf3d79cca80ef967773ba845d6
Gaston Dvoskin <dvoskingaston@gmail.com>
2024-10-15 08:23:10 -0300

    Responsive memberCard

--- src/components-reusable/MemberCard.tsx
+++ src/components-reusable/MemberCard.tsx
@@ -8,9 +8,9 @@ const MemberCard = ({member, setCurrentMember}: IMemberCardProps) => {
     member && (
       <div className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 flex flex-col text-center items-center p-4 bg-myWhite-100 w-11/12 max-w-sm shadow-2xl rounded-md z-10">
         <button className="absolute top-2 right-2 text-myBordeaux-500 border border-myBordeaux-500 p-1 hover:bg-myBordeaux-100 hover:text-myWhite-100" onClick={() => setCurrentMember(null)}><FaX /></button>
-        <h2 className="text-xl text-myPurple-900 font-bold animate-fadeIn">{member.name}</h2>
-        <img className="py-4 w-56 aspect-square object-cover" src={member.imageSrc} alt={`Primer plano de ${member.name}`} />
-        <p className="text-xs">{member.description}</p>
+        <h2 className="text-lg md:text-xl text-myPurple-900 font-bold animate-fadeIn">{member.name}</h2>
+        <img className="py-4 w-40 md:w-56 aspect-square object-cover" src={member.imageSrc} alt={`Primer plano de ${member.name}`} />
+        <p className="text-[10px] md:text-xs">{member.description}</p>
       </div>
     )
   )
