02698120852d6da75c6918b825ecf2e27f953cac
Gastón Dvoskin <dvoskingaston@gmail.com>
2025-08-26 10:06:26 -0300

    Delete old code in Xirgu

--- src/Xirgu.tsx
+++ src/Xirgu.tsx
@@ -2,11 +2,6 @@ import XirguSong from "./XirguSong";
 import songs from "./constants/xirguSongs";
 import andando from "./assets/images/songImages/andando.webp";
 import XirguCard from "./XirguCard";
-// import ImageComponent from "./components-reusable/ImageComponent";
-// import logo from "./assets/images/enterprise/logo.webp"; 
-// import logoVertical from "./assets/images/enterprise/logo-vertical.webp";
-
-// https://codesandbox.io/p/devbox/swiper-react-effect-cards-sw6ctl?file=%2Fsrc%2FApp.jsx%3A2%2C1-8%2C1
 
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
@@ -52,7 +47,6 @@ const Xirgu: React.FC = () => {
 
   return (
     <main className="h-dvh w-screen items-center bg-[conic-gradient(at_left,_#eeeeed,_#ffbcab)] flex flex-col text-center pt-12">
-      {/* <div className="bg-yellow-500 h-80 w-11"></div> */}
       <Swiper
         effect={'cards'}
         grabCursor={true}
@@ -71,23 +65,14 @@ const Xirgu: React.FC = () => {
           <XirguCard cardData={thanks} />
         </SwiperSlide>
       </Swiper>
-      {/* <a 
+      <a 
         href="https://www.cuerdosvocales.com" 
         className="text-blue-500 text-sm mt-2 underline hover:text-blue-700 transition-colors duration-300"
         target="_blank" 
         rel="noopener noreferrer"
       >
         cuerdosvocales.com
-      </a> */}
-
-              {/* <div className="h-full flex justify-between items-center">
-          <ImageComponent src={logo} alt="logo" className="h-16" />
-          <ImageComponent
-            src={logoVertical}
-            alt="Logo Vertical"
-            className="h-11"
-          />
-        </div> */}
+      </a>
     </main>
   );
 };
