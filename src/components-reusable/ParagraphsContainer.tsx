0e2277cf1d8bbd544e99e287e989b5329b3db836
Gastón Dvoskin <dvoskingaston@gmail.com>
2025-08-26 16:37:10 -0300

    Update news.ts

--- src/components-reusable/ParagraphsContainer.tsx
+++ src/components-reusable/ParagraphsContainer.tsx
@@ -2,9 +2,9 @@ import { IParagraphsContainerProps } from "../types";
 
 const ParagraphsContainer: React.FC<IParagraphsContainerProps> = ({ paragraphs }) => {
   return (
-    <div className={`w-full max-w-2xl px-3 pt-5 text-sm ${paragraphs.length > 1 ? "text-left" : "text-center"}`}>
+    <div className={`w-full max-w-2xl px-3 pt-5 text-sm text-balance text-center`}>
       {paragraphs.map((paragraph, index: number) => (
-        <p key={index} className="pb-2 ">
+        <p key={index} className="pb-2">
           {paragraph}
         </p>
       ))}
