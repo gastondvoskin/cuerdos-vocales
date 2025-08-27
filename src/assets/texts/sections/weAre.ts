32d4251d71886224ac0eb7e01c0ac87a8a2807cb
Gastón Dvoskin <dvoskingaston@gmail.com>
2025-08-27 20:22:11 -0300

    Update members in WeAre section

--- src/assets/texts/sections/weAre.ts
+++ src/assets/texts/sections/weAre.ts
@@ -1,17 +1,13 @@
 const weAreParagraphs = [
-  "Cuerdos Vocales es la agrupación musical fundada por Gastón Dvoskin en 2014 con el fin de explorar la fuerza del canto colectivo en el folklore argentino. Ha realizado ciclos de conciertos, peñas y talleres, presentándose junto a artistas de renombre como Juan Quintero, Teresa Parodi y La Charo. ", 
+  "Cuerdos Vocales es la agrupación musical fundada por Gastón Dvoskin en 2014 con el fin de explorar la fuerza del canto colectivo en el folklore argentino. Ha realizado ciclos de conciertos, peñas y talleres, presentándose junto a artistas de renombre como Juan Quintero, Teresa Parodi y La Charo. ",
 
-  "La formación actual cuenta con Gastón Dvoskin (dirección), Alejandro Randazzo (voz y producción), Bernardo Guarrochena (voz y producción), Manuel Estrach (voz), Alejo Trosman (voz), Lucrecia Flores (voz), Aldana Gómez (voz), Paula Bori (voz), Catalina Telerman (voz). ", 
+  "La formación actual cuenta con Aldi Gómez (voz, clarinete, wasá), Ale Randazzo (voz, guitarra, producción), Ale Trosman (voz, pandero), Ber Guarrochena (voz, audiovisuales), Cata Terlerman (voz, pandero), Gastón Dvoskin (percusión, dirección), Lucre Flores (voz, bombo), Manu Tirso (percusión, producción), Max Lukac (voz), Pau Bori (voz, shakers). ",
 
-  "En enero de 2020, Cuerdos Vocales se consagró como ganador del Pre Cosquín en la categoría conjunto vocal, lo que le permitió participar en la 60a edición del Festival Nacional de Folklore de Cosquín, donde actuó en el escenario Atahualpa Yupanqui. ", 
+  "En enero de 2020, Cuerdos Vocales se consagró como ganador del Pre Cosquín en la categoría conjunto vocal, lo que le permitió participar en la 60a edición del Festival Nacional de Folklore de Cosquín, donde actuó en el escenario Atahualpa Yupanqui. ",
 
   "Durante la pandemia, el grupo inició el ciclo de peñas La Cordura, declarado de Interés Cultural en dos oportunidades por la Ciudad de Buenos Aires, al igual que su proyecto Cuerdos Vocales con Invitadas. Asimismo, ha recibido el apoyo del Fondo Nacional de las Artes y del INAMU para la realización de arreglos, composiciones, grabaciones y conciertos. ",
 
-  "Cuerdos Vocales se ha presentado en importantes salas como Usina del Arte, Caras y Caretas, Café Vinilo, Centro Nacional de la Música y Teatro Nacional Cervantes. ",
+  "Cuerdos Vocales se ha presentado en importantes salas como Usina del Arte, Teatro Margarita Xirgu, Caras y Caretas, Café Vinilo, Centro Nacional de la Música y Teatro Nacional Cervantes. ",
+];
 
-  // "Redes: instagram.com/cuerdosvocales - facebook.com/cuerdosvocales - www.cuerdosvocales.com ",
-]
-
-
-
-export default weAreParagraphs; 
+export default weAreParagraphs;
