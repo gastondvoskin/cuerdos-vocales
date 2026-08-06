69b4505cd416ed26e20d2485829d566d52e8ef95
Gastón Dvoskin <dvoskingaston@gmail.com>
2026-08-06 19:45:30 -0300

    fix(members): update Chantal description

--- src/assets/texts/members/polonuer.ts
+++ src/assets/texts/members/polonuer.ts
@@ -2,7 +2,7 @@ import polonuerImage from "../../images/members/polonuerImage.png"
 
 const polonuer = {
   name: 'Chantal Polonuer',
-  description: 'Chantal Polonuer es cantante. Formó parte del ensamble de jazz vocal del espectáculo "Five to Swing", con un repertorio que recorre el swing, el bop y estándares clásicos del género. En 2025 participó como artista invitada en "¿De Quién Son Esos Ojitos?", tema de Magdalena Fleitas junto a Fabio Pérez, incluido en el álbum "Mardearena". Continúa profundizando su formación vocal explorando distintos géneros de la música popular. ',
+  description: 'Chantal Polonuer es cantante y ha incursionado en diversos estilos musicales, como el folklore, el tango, la música latinoamericana y el jazz. Se presenta en formato dúo con el Dúo Ni Nada, junto a Fabio Leonel Pérez, y con el Dúo Caeiro-Polonuer. Es cantante intérprete de la Orquesta de Folklore Manuel de Falla, bajo la dirección de Nahuel Quipildor, de la Orquesta de Tango del Falla y de Cuerdos Vocales. Forma parte de la banda de Magdalena Fleitas, con quien participó como cantante invitada en el disco "Mardearena", ganador de los Premios Gardel 2026. Se ha presentado en salas como el Centro Cultural Kirchner (CCK), el Auditorio de Belgrano, el Teatro Astengo de Rosario y el Centro Cultural 25 de Mayo. ',
   imageSrc: polonuerImage,
 }
 
